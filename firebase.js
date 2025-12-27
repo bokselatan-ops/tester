f38f9ca04140dbf37af1db3a5071f6b3b1236f4e
bokselatan-ops <bokselatan@gmail.com>
2025-12-27 15:27:49 +0700

    firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+export const firebaseConfig = {
+  apiKey: "API_KEY_KAMU",
+  authDomain: "sppd-dc087.firebaseapp.com",
+  projectId: "sppd-dc087",
+  storageBucket: "sppd-dc087.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+</script>
